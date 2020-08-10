c012e4fecc21ac9f2d6d27b3c1aa622a8d73452d
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-10 15:28:07 +0200

    Added user creation and login logic

---
+++ models/Profile.js
@@ -0,0 +1,103 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+    user: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    website: {
+        type: String
+    },
+    githubusername: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    experience: [{
+        title: {
+            type: String,
+            required: true
+        },
+        company: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true
+        },
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String
+        }
+    }],
+    education: [{
+        school: {
+            type: String,
+            required: true
+        },
+        fieldOfStudy: {
+            type: String,
+            required: true
+        },
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String
+        }
+    }],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = Profile = mongoose.model('Profile', profileSchema);
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,70 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Authenticate user
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async(req, res) => {
+    // We want the route to return the user's data
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   POST api/auth
+// @desc    LogIn user
+// @access  Public
+router.post(
+    '/', [body('password', 'Please, provide a password').notEmpty(), body('email', 'Please provide an email').notEmpty()],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            // See if an user with that email exists
+            let user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+            // Compare provided password with user.password
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+
+            // Return jsonwebtoken to navigate to private routes
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 3600 }, (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+);
 
 module.exports = router;
